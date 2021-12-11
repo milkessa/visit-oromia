616bbc748dc0f266792199996a0e691f31aab29b
unknown <milkessagebi@gmail.com>
2021-12-12 00:32:47 +0300

    New index.js file is created in Pages

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+    </>
+  );
+};
+
+export default Home;
