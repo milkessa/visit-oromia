7e118eff8245f2c6c0f44db26d841a62a5544253
unknown <milkessagebi@gmail.com>
2021-12-11 23:15:26 +0300

    Sidebar Modified

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 import { FaTimes } from "react-icons/fa";
+import { Link as LinkS } from "react-scroll";
+import { Link as LinkR } from "react-router-dom";
+
 export const SidebarContainer = styled.aside`
-position: fixed;
-z-index: 999;
-width: 100%;
-height: 100%;
-background: #0d0d0d;
-display: grid;
-top: 0;
-left: 0;
-transition: 0.3s ease-in-out;
-opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")}
-top:${({ isOpen }) => (isOpen ? "0" : "-100")};
-    
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: #0d0d0d;
+  display: grid;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  /* opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")}
+top:${({ isOpen }) => (isOpen ? "0" : "-100")}; */
 `;
 
 export const CloseIcon = styled(FaTimes)`
@@ -26,4 +28,47 @@ export const Icon = styled.div`
   background: transparent;
   cursor: pointer;
   outline: none;
+`;
+
+export const SidebarWrapper = styled.div`
+  color: #fff;
+`;
+export const SidebarLink = styled(LinkS)`
+  display: felx;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    color: #01bf71;
+    transition: 0.2s ease-in-out;
+  }
+`;
+export const sideBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+export const SidebarRoute = styled(LinkR)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 16px 64px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+  }
 `;
